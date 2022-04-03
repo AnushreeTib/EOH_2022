082f980231fa66ddee6eb5b4f21051e4618a5241
meghanlu <meghan7@DESKTOP-8GA8H5B.localdomain>
2022-04-03 16:41:41 -0500

    Finishing connecting model to frontend

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def main():
   return render_template('index.html', test = "")
+
 @app.route("/", methods=['post'])
-def firstPagePost():
-    
-  text = input()
+def POST_text():
+  #print("hello")    
+  text = request.form['to_summarise']
   
   if len(text) > 0:
     out = output(text)
-    print(out)
-    
-    return render_template('index.html', out = out, text = text) 
+
+    return render_template('index.html', out = out, text = text), 200
     
   else: 
-    return render_template('index.html')
+    return render_template('index.html'), 200
